5d5cc53596087e44690ac42dbd655e894990315f
atsukoon <atsuko.o.d22@gmail.com>
2024-12-30 15:43:02 +0900

    Internet Identityを使用したログイン機能 実装

--- src/token_frontend/src/main.jsx
+++ src/token_frontend/src/main.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './main.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { AuthClient } from "@dfinity/auth-client";
+import "./main.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+const init = async () => {
+  const authClient = await AuthClient.create();
+
+  if (authClient.isAuthenticated()) {
+    handleAuthenticated(authClient);
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthenticated(authClient);
+      },
+    });
+  }
+};
+
+async function handleAuthenticated(authClient) {
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+init();
